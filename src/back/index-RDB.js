5250f845669ae9535ee82efb4086ee3c9bfad1fe
Raúl_de_los_Reyes <rauldelosreyes27@gmail.com>
2026-03-13 10:04:27 +0100

    cambio a modulo

--- src/back/index-RDB.js
+++ src/back/index-RDB.js
@@ -1,14 +1,14 @@
 //=======================================//
 //============ FUNCIONES RDB ============//
 //=======================================//
-
+export function loadBackendRDB(app){
 const BASE_RDB = "/api/v1/workers-productivity";
 
 const db_RDB = new Datastore({
   filename: "workers-productivity.db",
   autoload: true
 });
-export function loadBackendRDB(app){
+
 db_RDB.ensureIndex({ fieldName: "key", unique: true }, (err) => {
   if (err) {
     console.error("Error creating NeDB index for RDB:", err);
