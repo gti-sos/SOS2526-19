861c127856501e3e342a0a8f187a3aa64a54d04b
Pablo <pabrodarm@alum.us.es>
2026-04-21 17:14:10 +0200

    grafica drought-stats

--- src/front/src/routes/analytics/drought-stats/+page.svelte
+++ src/front/src/routes/analytics/drought-stats/+page.svelte
@@ -2,324 +2,84 @@
     import Highcharts from 'highcharts';
     import { onMount } from 'svelte';
 
-    
+    let myData = $state();
 
     onMount(async () => {
+        const res = await fetch("/api/v1/drought-stats");
+        myData = await res.json();
+
+        // @ts-ignore
+        const grouped = myData.reduce((acc, item) => {
+            const country = item.country;
+
+            if (!acc[country]) {
+                acc[country] = {
+                    severity: 0,
+                    duration: 0
+                };
+            }
+
+            acc[country].severity += item.severity_km2;
+            acc[country].duration += item.duration_day;
+
+            return acc;
+        }, {});
+
+        const countries = Object.keys(grouped);
+
+        const severityData = countries.map(country => grouped[country].severity);
+        const durationData = countries.map(country => grouped[country].duration);
+
         // @ts-ignore
         Highcharts.chart('container', {
             chart: {
-                type: 'column'
+                type: 'bar',
+                reflow: true
             },
             title: {
-                text: 'Browser market shares. January, 2022'
-            },
-            subtitle: {
-                text: 'Click the columns to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
-            },
-            accessibility: {
-                announceNewData: {
-                    enabled: true
-                }
+                text: 'Drought stats by country'
             },
             xAxis: {
-                type: 'category'
-            },
-            yAxis: {
+                categories: countries,
                 title: {
-                    text: 'Total percent market share'
+                    text: 'Country'
                 }
-
             },
-            legend: {
-                enabled: false
+            yAxis: [{
+                title: {
+                    text: 'Severity (km²)'
+                }
+            }, {
+                title: {
+                    text: 'Duration (days)'
+                },
+                opposite: true
+            }],
+            tooltip: {
+                shared: true
             },
             plotOptions: {
-                series: {
-                    borderWidth: 0,
+                bar: {
                     dataLabels: {
-                        enabled: true,
-                        format: '{point.y:.1f}%'
+                        enabled: true
                     }
                 }
             },
-
-            tooltip: {
-                headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-                pointFormat: '<span style="color:{point.color}">{point.name}</span>: ' +
-                    '<b>{point.y:.2f}%</b> of total<br/>'
-            },
-
             series: [
                 {
-                    name: 'Browsers',
-                    colorByPoint: true,
-                    data: [
-                        {
-                            name: 'Chrome',
-                            y: 63.06,
-                            drilldown: 'Chrome'
-                        },
-                        {
-                            name: 'Safari',
-                            y: 19.84,
-                            drilldown: 'Safari'
-                        },
-                        {
-                            name: 'Firefox',
-                            y: 4.18,
-                            drilldown: 'Firefox'
-                        },
-                        {
-                            name: 'Edge',
-                            y: 4.12,
-                            drilldown: 'Edge'
-                        },
-                        {
-                            name: 'Opera',
-                            y: 2.33,
-                            drilldown: 'Opera'
-                        },
-                        {
-                            name: 'Internet Explorer',
-                            y: 0.45,
-                            drilldown: 'Internet Explorer'
-                        },
-                        {
-                            name: 'Other',
-                            y: 1.582,
-                            drilldown: null
-                        }
-                    ]
-                }
-            ],
-            drilldown: {
-                breadcrumbs: {
-                    position: {
-                        align: 'right'
-                    }
+                    name: 'Total Severity (km²)',
+                    data: severityData,
+                    yAxis: 0
                 },
-                series: [
-                    {
-                        name: 'Chrome',
-                        id: 'Chrome',
-                        data: [
-                            [
-                                'v65.0',
-                                0.1
-                            ],
-                            [
-                                'v64.0',
-                                1.3
-                            ],
-                            [
-                                'v63.0',
-                                53.02
-                            ],
-                            [
-                                'v62.0',
-                                1.4
-                            ],
-                            [
-                                'v61.0',
-                                0.88
-                            ],
-                            [
-                                'v60.0',
-                                0.56
-                            ],
-                            [
-                                'v59.0',
-                                0.45
-                            ],
-                            [
-                                'v58.0',
-                                0.49
-                            ],
-                            [
-                                'v57.0',
-                                0.32
-                            ],
-                            [
-                                'v56.0',
-                                0.29
-                            ],
-                            [
-                                'v55.0',
-                                0.79
-                            ],
-                            [
-                                'v54.0',
-                                0.18
-                            ],
-                            [
-                                'v51.0',
-                                0.13
-                            ],
-                            [
-                                'v49.0',
-                                2.16
-                            ],
-                            [
-                                'v48.0',
-                                0.13
-                            ],
-                            [
-                                'v47.0',
-                                0.11
-                            ],
-                            [
-                                'v43.0',
-                                0.17
-                            ],
-                            [
-                                'v29.0',
-                                0.26
-                            ]
-                        ]
-                    },
-                    {
-                        name: 'Firefox',
-                        id: 'Firefox',
-                        data: [
-                            [
-                                'v58.0',
-                                1.02
-                            ],
-                            [
-                                'v57.0',
-                                7.36
-                            ],
-                            [
-                                'v56.0',
-                                0.35
-                            ],
-                            [
-                                'v55.0',
-                                0.11
-                            ],
-                            [
-                                'v54.0',
-                                0.1
-                            ],
-                            [
-                                'v52.0',
-                                0.95
-                            ],
-                            [
-                                'v51.0',
-                                0.15
-                            ],
-                            [
-                                'v50.0',
-                                0.1
-                            ],
-                            [
-                                'v48.0',
-                                0.31
-                            ],
-                            [
-                                'v47.0',
-                                0.12
-                            ]
-                        ]
-                    },
-                    {
-                        name: 'Internet Explorer',
-                        id: 'Internet Explorer',
-                        data: [
-                            [
-                                'v11.0',
-                                6.2
-                            ],
-                            [
-                                'v10.0',
-                                0.29
-                            ],
-                            [
-                                'v9.0',
-                                0.27
-                            ],
-                            [
-                                'v8.0',
-                                0.47
-                            ]
-                        ]
-                    },
-                    {
-                        name: 'Safari',
-                        id: 'Safari',
-                        data: [
-                            [
-                                'v11.0',
-                                3.39
-                            ],
-                            [
-                                'v10.1',
-                                0.96
-                            ],
-                            [
-                                'v10.0',
-                                0.36
-                            ],
-                            [
-                                'v9.1',
-                                0.54
-                            ],
-                            [
-                                'v9.0',
-                                0.13
-                            ],
-                            [
-                                'v5.1',
-                                0.2
-                            ]
-                        ]
-                    },
-                    {
-                        name: 'Edge',
-                        id: 'Edge',
-                        data: [
-                            [
-                                'v16',
-                                2.6
-                            ],
-                            [
-                                'v15',
-                                0.92
-                            ],
-                            [
-                                'v14',
-                                0.4
-                            ],
-                            [
-                                'v13',
-                                0.1
-                            ]
-                        ]
-                    },
-                    {
-                        name: 'Opera',
-                        id: 'Opera',
-                        data: [
-                            [
-                                'v50.0',
-                                0.96
-                            ],
-                            [
-                                'v49.0',
-                                0.82
-                            ],
-                            [
-                                'v12.1',
-                                0.14
-                            ]
-                        ]
-                    }
-                ]
-            }
+                {
+                    name: 'Total Duration (days)',
+                    data: durationData,
+                    yAxis: 1
+                }
+            ]
         });
 
     })
 </script>
 
-<div id='container'></div>
+<div id="container"></div>
