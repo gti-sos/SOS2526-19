25b6c68a681f51807c66396c647a3c330e5b24ea
Raúl_de_los_Reyes <rauldelosreyes27@gmail.com>
2026-05-14 13:06:10 +0200

    Video y efforts Raul

--- src/front/src/routes/about/+page.svelte
+++ src/front/src/routes/about/+page.svelte
@@ -1,4 +1,5 @@
 <h1>Enlaces a los videos</h1>
 <a href="https://youtu.be/VklcquV3YiQ">Enlace Pablo</a>
 <p>Enlace Raúl</p>
+<a href="https://youtu.be/u4NKpHTY4QY">Enlace Raúl</a>
 <p>Enlace Javier</p>
