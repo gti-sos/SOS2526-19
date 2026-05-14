1ee20406266b6171a2bfe849e3d8fc244aa26d1e
Raúl_de_los_Reyes <rauldelosreyes27@gmail.com>
2026-05-14 13:14:10 +0200

    arreglo tonto

--- src/front/src/routes/about/+page.svelte
+++ src/front/src/routes/about/+page.svelte
@@ -1,5 +1,4 @@
 <h1>Enlaces a los videos</h1>
 <a href="https://youtu.be/VklcquV3YiQ">Enlace Pablo</a>
-<p>Enlace Raúl</p>
 <a href="https://youtu.be/u4NKpHTY4QY">Enlace Raúl</a>
 <p>Enlace Javier</p>
