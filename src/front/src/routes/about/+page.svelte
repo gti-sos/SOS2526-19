26f439e807f33a043e7d077df9557234a65397b9
Javier Morales Jiménez <146114760+JaviMJ2@users.noreply.github.com>
2026-05-14 19:41:01 +0200

    DONE

--- src/front/src/routes/about/+page.svelte
+++ src/front/src/routes/about/+page.svelte
@@ -1,4 +1,4 @@
 <h1>Enlaces a los videos</h1>
 <a href="https://youtu.be/VklcquV3YiQ">Enlace Pablo</a>
 <a href="https://youtu.be/u4NKpHTY4QY">Enlace Raúl</a>
-<p>Enlace Javier</p>
+<a href="https://youtu.be/b-kXw-kR03k">Enlace Javier</a>
